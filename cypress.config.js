a03c8b650b57946fabd6ed82c7a1e54f1194af03
Tarsis <br.tmvdl@gmail.com>
2024-07-23 18:15:31 -0300

    Updating app files

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,5 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     projectId: '5djfo1',
-    baseUrl: 'http://192.168.10.1',
-    setupNodeEvents(on, config) { },
   },
 })
--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,11 @@
 describe('change pwd', () => {
-  it('opens browser', () => {
-    cy.visit('/')
+  const config = { ssid: '', pwd: '' }
+
+  it('logs in', () => {
+    cy.visit('http://192.168.10.1')
+    cy.get('#username').type('admin')
+    cy.get('#password').type('admin')
+    cy.get('#login').click()
   })
+
 })
